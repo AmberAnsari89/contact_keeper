78393e27c240b27e5eea8709774df581a627c99b
AMBER MURTUZA ANSARI <amberansari10@gmail.com>
2022-05-23 01:43:20 +0530

    setting.py

--- contact_keeper/settings.py
+++ contact_keeper/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1navi0gn-rn+pe+yk-xjd-1h6eq2wao6x%^znjpt&p!2l58ru-'
+#SECRET_KEY = 'django-insecure-1navi0gn-rn+pe+yk-xjd-1h6eq2wao6x%^znjpt&p!2l58ru-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
